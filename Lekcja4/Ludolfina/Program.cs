422d46c66ab2f78f0292f6ac1f5dba22a4682160
NickyMB <82083134+NickyMB@users.noreply.github.com>
2023-04-20 10:33:21 +0200

    Lekcja4

--- Lekcja4/Ludolfina/Program.cs
+++ Lekcja4/Ludolfina/Program.cs
@@ -8,16 +8,38 @@ namespace Ludolfina
 {
     interface ILeibniz
     {
-        int liczba { get; }
+        double Ludolfina(int n);
     }
     interface IEuler
     {
-        int liczba { get; }
+        double Ludolfina(int n);
+    }
+    class Obliczenia : ILeibniz , IEuler
+    {
+        
+        double ILeibniz.Ludolfina(int n)
+        {
+            double lud = 0;
+            for (int i = 0; i < n; i++)
+                lud += Math.Pow(-1, i) / (2 * i + 1);
+            return lud*4;
+        }
+        double IEuler.Ludolfina(int n)
+        {
+            double lud = 0;
+            for (int i = 1; i <= n; i++)
+                lud += 1 / Math.Pow(i, 2);
+            return Math.Sqrt(lud * 6);
+        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Obliczenia ob1 = new Obliczenia();
+            Console.WriteLine(Math.PI - ((ILeibniz) (ob1)).Ludolfina(20));
+            Console.WriteLine(Math.PI - ((IEuler) (ob1)).Ludolfina(20));
+            Console.ReadKey();
         }
     }
 }
