68cd29646fdeb35689dea6f400cdad5d739f20c3
NickyMB <82083134+NickyMB@users.noreply.github.com>
2023-04-20 08:55:47 +0200

    Lekcja4

--- Lekcja4/CzworobokV2/Program.cs
+++ Lekcja4/CzworobokV2/Program.cs
@@ -6,10 +6,63 @@ using System.Threading.Tasks;
 
 namespace CzworobokV2
 {
-    internal class Program
+    public abstract class Czworobok
     {
-        static void Main(string[] args)
+        protected int x;
+        protected int y;
+        protected double z;
+        public Czworobok(int bok_a, int bok_b)
         {
+            x = bok_a;
+            y = bok_b;
+        }
+        public abstract double Pole();
+    }
+    
+    public class Kwadrat : Czworobok
+    {
+        public Kwadrat(int bok_a)
+            : base(bok_a, bok_a)
+        {
+        }
+        public override double Pole()
+        {
+            return x * y;
+        }
+    }
+    public class Trapez : Czworobok
+    {
+        public Trapez(int bok_a, int bok_b, double bok_c):base(bok_a,bok_b)
+        {
+            z = bok_c;
+        }
+        public override double Pole()
+        {
+            return ((x + y)*z)/2;
+        }
+    }
+    public class Prostokat : Czworobok
+    {
+        public Prostokat(int bok_a, int bok_b): base(bok_a, bok_b)
+        {
+        }
+        public override double Pole()
+        {
+            return x * y;
+
+        }
+    }
+    class pokazik
+    {
+        public static void Main()
+        {
+            Czworobok kw = new Kwadrat(5);
+            Czworobok pr = new Prostokat(5, 4);
+            Czworobok tr = new Trapez(7,8,5);
+            Czworobok[] tab = { kw, pr, tr };
+            for (int i = 0; i < tab.Length; i++)
+                Console.WriteLine(tab[i].Pole());
+            Console.ReadLine();
         }
     }
 }
